801c3106bfdd94f814f499030ae2499709457905
chinki kumari singh <singhchinkikumari.com>
2022-08-12 20:20:53 +0530

    [Chinki] Use  of ASP.NET Web API

---
+++ ProductsApp/ProductsApp/Controllers/ItemsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductsApp.Models;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+       readonly Product[] products = new Product[]
+       {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+       };
+        [Microsoft.AspNetCore.Mvc.HttpGet("ProductsDetails")]
+        public  IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+        [Microsoft.AspNetCore.Mvc.HttpGet("ProductsItemDetails")]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return (IHttpActionResult)NotFound();
+            }
+            
+            return (IHttpActionResult)Ok(product);
+        }
+    }
+}
---
+++ ProductsApp/ProductsApp/JavaScript.js
@@ -0,0 +1,10 @@
+﻿ function find() {
+    var id = $('#prodId').val();
+    $.getJSON(apiUrl + '/' + id)
+        .done(function (data) {
+            $('#product').text(formatItem(data));
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $('#product').text('Error: ' + err);
+        });
+}
